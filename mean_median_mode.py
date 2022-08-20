893cf6bca0c0c4a2ae94a5201995ee7148eb0f28
Manoj M Mallya <94393300+mixed-farming@users.noreply.github.com>
2022-08-20 10:38:18 +0530

    Update mean_median_mode.py

--- mean_median_mode.py
+++ mean_median_mode.py
@@ -6,7 +6,7 @@ a = [3,2,2,1,3,4,3,1,1,3]
 print('Mean of the array = ', np.mean(a)) # st.mean() works
 # Mean = sum of array elements/number of array elements
 
-print('Median of the array = ',np.median(a)) # st.mode() works
+print('Median of the array = ',np.median(a)) # st.median() works
 # Median = middle term if total no. of terms are odd.
 # Median = Average of the terms in the middle (if total no. of terms are even)
 
