ea867771bc460773e575fb2399a24ac0b765c480
Manoj M Mallya <94393300+mixed-farming@users.noreply.github.com>
2022-08-20 10:34:21 +0530

    Update array_sum.py

--- array_sum.py
+++ array_sum.py
@@ -1,3 +1,4 @@
+import numpy as np
 def sum_array(arr):
     sum=0
     for i in arr:
@@ -8,3 +9,4 @@ arr=[1,2,3,4,5]
 # n=len(arr)
 result=sum_array(arr)
 print('Sum of the array  : ',result)
+print('Numpy array sum : ',np.sum(arr))
