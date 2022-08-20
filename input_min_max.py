cca0c285f2477ff6bc7c91a5770d20a466552e17
Manoj M Mallya <94393300+mixed-farming@users.noreply.github.com>
2022-08-21 00:30:44 +0530

    Add files via upload

---
+++ input_min_max.py
@@ -0,0 +1,19 @@
+largest = None
+smallest = None
+
+while True:
+    num = input("Enter a number: ")
+    if num == "done":
+        break
+    try:
+        num = int(num)
+        if largest is None or largest < num:
+            largest = num
+        elif smallest is None or smallest > num:
+            smallest = num
+    except:
+        print("Invalid input")
+        continue
+
+print("Maximum is", largest)
+print("Minimum is", smallest)
