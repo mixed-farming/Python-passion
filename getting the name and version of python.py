2007aa29906f255531834b6af0cf07cd33915348
Manoj M Mallya <94393300+mixed-farming@users.noreply.github.com>
2022-09-02 13:52:17 +0530

    Add files via upload

---
+++ getting the name and version of python.py
@@ -0,0 +1,13 @@
+# Displaying the name and version of python using str.format method in print statement
+
+import sys
+print("Python version name : {}".format(sys.version))
+print("Version in use : {}".format(sys.version_info))
+
+'''
+
+'sys' module provides access to some variables used or maintained by the interpreter
+and to functions that interact strongly with the interpreter.
+
+'''
+
