a046a740985a2162c3e14d8c64da4ed962578720
Manoj M Mallya <94393300+mixed-farming@users.noreply.github.com>
2022-10-04 01:23:22 +0530

    Add files via upload

---
+++ calendar_month.py
@@ -0,0 +1,6 @@
+import calendar_month
+yy=2022 #year
+mm=10 #month
+#display the calendar
+print(calendar_month.month(yy, mm))
+
---
+++ calendar_year.py
@@ -0,0 +1,5 @@
+import calendar
+yyyy=2022 #year
+#display the calendar
+print(calendar.calendar(yyyy,3,1,7))#parameters - year, column gap, row gap, gap between month blocks
+
