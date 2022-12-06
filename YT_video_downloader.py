2ce04174a29f26076bd0ca7714122e6a81e95cf3
Manoj M Mallya <94393300+mixed-farming@users.noreply.github.com>
2022-12-06 21:32:49 +0530

    Add files via upload

---
+++ YT_video_downloader.py
@@ -0,0 +1,17 @@
+#pip install pytube
+from pytube import YouTube
+import os
+from pathlib import Path
+
+link = input("Enter YouTube video link here: ")
+
+url = YouTube(link)
+
+print("\nDownloading...Please wait!")
+
+video = url.streams.get_highest_resolution()
+
+# path_to_download_folder = str(os.path.join(Path.home(), "Downloads")) # -> In the downloads folder
+path_to_download_folder = "D:/"
+video.download(path_to_download_folder)
+print("\nDownload complete :)\nYou can find the video at : ",path_to_download_folder)
